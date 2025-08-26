d1ceaaf41137c461d8c4e9d267bf170fa6348c63
bigyan-sapkota <bigyansapkotacodes@gmail.com>
2025-08-26 17:13:37 +0545

    add practice code

---
+++ 01_basics/06_practice.dart
@@ -0,0 +1,70 @@
+// DECLARE AND INITIALIZE VARIABLES --------------------------- 
+
+// void main(){
+//     int age = 25;
+//     double height = 160.7;
+//     String name = "Binaya Joe";
+//     bool isStudent= false;
+
+//     print("Name $name");
+//     print("Age: $age");
+//     print("Height: $height");
+//     print("Is a student : $isStudent");
+// }
+
+// EXPLORE DIFFERENT DATA TYPES --------------------------------
+
+// void main(){
+//     List<String> fruits = ["Apple", "Banana", "Cherry"];
+//     Map<String, int> scores= {"Alice" : 90, "Bob" : 85, "Charlie" : 95};
+//     String? nullableString = null;
+
+//     print("Fruits $fruits");
+//     print("Score of Alice: ${scores['Alice']}");
+//     print("Nullabe string: $nullableString");
+// }
+
+// UNDERSTAND TYPE INFERENCE AND ANNOTATIONS--------------------------------
+
+// void main(){
+//     var city = "New York";
+//     String country = 'USA';
+//     print("City : $city");
+//     print("Country : $country");
+// }
+
+
+// Use of constants and final variables
+// void main(){
+//     const double pi = 3.14;
+//     final DateTime currentTime = DateTime.now();
+
+//     print('Pi: $pi');
+//     print("Current Time : $currentTime");
+// }
+
+// DEFINE AND CALL FUNCTIONS------------------------------------------
+// void greet(String name){
+//     print("Hello $name");
+// }
+
+// void main(){
+//     greet("Alice");
+// }
+
+// WORK WITH METHODS IN CLASSES----------------------------------------------
+// class Person{
+//     String name;
+//     int age;
+
+//     Person(this.name, this.age);
+
+//     void displayInfo(){
+//         print("The name of person is : $name and his/her age is $age");
+//     }
+// }
+
+// void main(){
+//     Person person = Person("Bigyan Sapkota", 23);
+//     person.displayInfo();
+// }
